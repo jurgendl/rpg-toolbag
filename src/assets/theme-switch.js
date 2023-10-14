0ef9eec3f53bf4a2742e92fa74a266c49d272d48
jurgen.de.landsheer <jurgen.de.landsheer@gmail.com>
2023-10-14 15:42:00 +0200

    rpg-toolbag

---
+++ src/assets/theme-switch.js
@@ -0,0 +1,20 @@
+$(".darkmode").click(function () {
+    $("body").toggleClass("dark");
+    if ($("body").hasClass("dark")) {
+        document.getElementById("theme").href = "assets/darkly.bootstrap.min.css";
+        $(".no-thead-dark")
+            .removeClass("no-thead-dark")
+            .addClass("thead-dark");
+        $(".no-table-dark")
+            .removeClass("no-table-dark")
+            .addClass("table-dark");
+    } else {
+        document.getElementById("theme").href = "assets/original.bootstrap.min.css";
+        $(".thead-dark")
+            .removeClass("thead-dark")
+            .addClass("no-thead-dark");
+        $(".table-dark")
+            .removeClass("table-dark")
+            .addClass("no-table-dark");
+    }
+});
